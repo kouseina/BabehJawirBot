38847a30bbf880cdff3a85f3876a241c7deacbc2
kouseina <daffa@diyo.app>
2023-09-22 07:22:19 +0700

    feat: add mention all user

--- index.js
+++ index.js
@@ -12,3 +12,27 @@ client.on('ready', () => {
 });
 
 client.initialize();
+
+client.on('message', async (message) => {
+    console.log(message.body);
+
+    if (message.body === '!ping') {
+        message.reply('pong');
+    }
+
+    if (message.body === '!everyone') {
+        const chat = await message.getChat();
+
+        let text = "";
+        let mentions = [];
+
+        for (let participant of chat.participants) {
+            const contact = await client.getContactById(participant.id._serialized);
+
+            mentions.push(contact);
+            text += `@${participant.id.user} `;
+        }
+
+        await chat.sendMessage(text, { mentions });
+    }
+});
